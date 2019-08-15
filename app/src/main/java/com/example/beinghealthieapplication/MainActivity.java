47c54da1bd802581f31f3c0a556017ffdd5fde31
arjita mahajan <arjitam@andrew.cmu.edu>
2019-08-15 05:40:04 +0530

    app

---
+++ app/src/main/java/com/example/beinghealthieapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.beinghealthieapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
